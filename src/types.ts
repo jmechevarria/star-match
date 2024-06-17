b3dcf1f850be8b9ca0774ede49c06dbe2e4a3197
Jesús Miguel Echevarría Díaz <jmechevarria2015@gmail.com>
2024-06-17 17:16:31 +0200

    feat: - define types, utilities and styles

---
+++ src/types.ts
@@ -0,0 +1,12 @@
+export enum NumberStatus {
+  AVAILABLE = "available",
+  USED = "used",
+  CANDIDATE = "candidate",
+  WRONG = "wrong",
+}
+
+export enum GameStatus {
+  ACTIVE = "active",
+  WON = "won",
+  LOST = "lost",
+}
